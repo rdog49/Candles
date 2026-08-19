61f2e3dc332103a420cee97c95dbf83f5fcb0876
VladimirSA <vladimisav@gmail.com>
2026-08-19 18:32:37 +0600

    redacted styles for feedback-block

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ const menumobile = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', ()=> {
     menumobile.classList.toggle('menu--open');
 });
+
+const swiperOne  = new Swiper('.feedback__slider', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
